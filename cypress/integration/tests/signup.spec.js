aa275ac5ce73fc566d3ff7a93c2f206e63bdc6c8
Yuliya Kozina <yuliya.kozina@keenethics.com>
2021-01-08 14:30:58 +0200

    Add signup

--- cypress/integration/tests/signup.spec.js
+++ cypress/integration/tests/signup.spec.js
@@ -3,12 +3,15 @@ it('Signup', () => {
     cy.fixture('secret').then(({ FirstName, LastName, email, pass, ruusterHomePage, phoneNumber }) => {
         cy.log('Go to Home page');
         cy.visit(ruusterHomePage);
-        cy.get('input[id="signup-first-name"]').type(FirstName);
-        cy.get('input[id="signup-last-name"]').type(LastName);
-        cy.get('input[id="signup-email"]').type(email);
-        cy.get('input[id="signup-password"]').type(pass);
-        cy.get('button[type="submit".').click();
-        cy.get('input[id="secure-account-phone-number")').type(phoneNumber);
-        cy.get('button[type="submit".').click();
+        cy.get('.button').eq(1).click();
+        cy.get('input[name="firstName"]').type(FirstName,{force:true});
+        cy.get('input[name="lastName"]').type(LastName, {force:true});
+        cy.get('input[name="email"]').type(email, {force:true});
+        cy.get('input[name="password"]').type(pass, {force:true});
+        //cy.get('button[type="submit"]').should('be.visible');
+        cy.get('button[type="submit"]').click();
+        cy.wait(6000);
+        // cy.get('input[name="phone-number")').type(phoneNumber, {force:true});
+        // cy.get('button[type="submit".').click({force:true});
     });
 });
