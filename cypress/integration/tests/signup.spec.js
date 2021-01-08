6617a2a0aab2b80f0b6b0ad3ed821e08e037e871
Yuliya Kozina <yuliya.kozina@keenethics.com>
2021-01-08 15:27:16 +0200

    Modified sign up

--- cypress/integration/tests/signup.spec.js
+++ cypress/integration/tests/signup.spec.js
@@ -8,10 +8,9 @@ it('Signup', () => {
         cy.get('input[name="lastName"]').type(LastName, {force:true});
         cy.get('input[name="email"]').type(email, {force:true});
         cy.get('input[name="password"]').type(pass, {force:true});
-        //cy.get('button[type="submit"]').should('be.visible');
-        cy.get('button[type="submit"]').click();
+        cy.get('button[type="submit"]').click({force:true});
         cy.wait(6000);
-        // cy.get('input[name="phone-number")').type(phoneNumber, {force:true});
-        // cy.get('button[type="submit".').click({force:true});
+        cy.get('input[name="phone-number"]').type(phoneNumber, {force:true});
+        cy.get('button[type="submit"]').click({force:true});
     });
 });
