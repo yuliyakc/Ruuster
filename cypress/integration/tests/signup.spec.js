3cd8ff29d0c88a0203a5f7dc39c1374251b4a3d2
Yuliya Kozina <yuliya.kozina@keenethics.com>
2021-01-18 13:18:28 +0200

    Modified sign up

--- cypress/integration/tests/signup.spec.js
+++ cypress/integration/tests/signup.spec.js
@@ -1,27 +1,39 @@
 
 describe('Verify sign up', function() {
-  it('Signup', function() {
-    cy.fixture('secret').then((secretData) => {
-    var i = 0;
-    for (i = 0; i < 2 ; i++) {
-        cy.log("Go to Home page");
-        cy.visit(secretData.ruusterHomePage);
-        cy.get('.button').eq(1).click();
-        cy.get('input[name="firstName"]').type(secretData.FirstName, {force: true});
-        cy.get('input[name="lastName"]').type(secretData.LastName, {force: true});
-        secretData.lastEmailNum = parseInt(secretData.lastEmailNum) + 1;
-        console.log(secretData.lastEmailNum);
-        //cy.writeFile('D:/__PROJECTS__/Ruuster/cypress/fixtures/secret.json', JSON.stringify(secretData));
-        cy.writeFile('./cypress/fixtures/secret.json', JSON.stringify(secretData));
-        let emailForCode = secretData.email.replace("@", "+" + secretData.lastEmailNum + "@");
-        cy.get('input[name="email"]').type(emailForCode, {force: true});
-        cy.get('input[name="password"]').type(secretData.pass, {force: true});
-        cy.get('button[type="submit"]').click({force: true});
-        cy.wait(6000);
-      }
-        cy.get('input[name="phone-number"]').type(secretData.phoneNumber, {force:true});
-        cy.get('button[type="submit"]').click({force:true});
-      });
+    it('Signup', function () {
+        cy.fixture('secret').then((secretData) => {
+            var i = 0;
+            for (i = 0; i < 2; i++) {
+                cy.log("Go to Home page");
+                cy.visit(secretData.ruusterHomePage);
+                cy.get('.button').eq(1).click();
+                cy.get('input[name="firstName"]').type(secretData.FirstName, {force: true});
+                cy.get('input[name="lastName"]').type(secretData.LastName, {force: true});
+                secretData.lastEmailNum = parseInt(secretData.lastEmailNum) + 1;
+                console.log(secretData.lastEmailNum);
+                //cy.writeFile('D:/__PROJECTS__/Ruuster/cypress/fixtures/secret.json', JSON.stringify(secretData));
+                cy.writeFile('./cypress/fixtures/secret.json', JSON.stringify(secretData));
+                let emailForCode = secretData.email.replace("@", "+" + secretData.lastEmailNum + "@");
+                cy.get('input[name="email"]').type(emailForCode, {force: true});
+                cy.get('input[name="password"]').type(secretData.pass, {force: true});
+                cy.get('button[type="submit"]').click({force: true});
+                cy.wait(6000);
+            }
+            cy.get('input[name="phone-number"]').type(secretData.phoneNumber, {force: true});
+            cy.get('button[type="submit"]').click({force: true});
+            cy.wait(10000);
+            cy.request('http://localhost:3000/get_code_from_twillo')
+                .then((response) => {
+                    console.log(response.body[0].body.substring(0, 6));
+                    let code = response.body[0].body.substring(0, 6);
+                    cy.log(code);
+                    cy.wait(5000);
+                    cy.get('input[id="secure-account-code"]').type(code, {force: true});
+                    cy.get('.button').click({force: true});
+                    cy.wait(3000);
+                    cy.get('.button').click({force: true});
+                });
+        });
     });
 });
 
