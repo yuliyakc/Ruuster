2a31fc861edf50c379f2cf2d2d252ff3330069ed
Yuliya Kozina <yuliya.kozina@keenethics.com>
2021-02-24 15:35:05 +0200

    Check notification

--- cypress/integration/tests/signin.spec.js
+++ cypress/integration/tests/signin.spec.js
@@ -1,34 +1,78 @@
 describe('Sign in', function() {
-    it('Signin', function () {
+
+    it('SigninAgent', function () {
         cy.fixture('secret').then((secretData) => {
             cy.log("Go to Home page");
             cy.visit(secretData.ruusterHomePageSignin);
-            cy.get('input[name="firstName"]').type(secretData.agentFirstName, {force: true});
-            cy.get('input[name="lastName"]').type(secretData.agentLastName, {force: true});
-            secretData.lastEmailNum = parseInt(secretData.lastEmailNum) + 1;
-                console.log(secretData.lastEmailNum);
-                //cy.writeFile('D:/__PROJECTS__/Ruuster/cypress/fixtures/secret.json', JSON.stringify(secretData));
-                cy.writeFile('./cypress/fixtures/secret.json', JSON.stringify(secretData));
-                let emailForCode = secretData.email.replace("@", "+" + secretData.lastEmailNum + "@");
-                cy.get('input[name="email"]').type(emailForCode, {force: true});
-                cy.get('input[name="password"]').type(secretData.pass, {force: true});
-                cy.get('button[type="submit"]').click({force: true});
-                cy.wait(6000);
-            }
-            cy.get('input[name="phone-number"]').type(secretData.phoneNumber, {force: true});
+            cy.get('input[name="email"]').type(secretData.agentEmail, {force: true});
+            cy.get('input[name="password"]').type(secretData.agentPass, {force: true});
             cy.get('button[type="submit"]').click({force: true});
-            cy.wait(10000);
+            cy.wait(5000);
             cy.request('http://localhost:3000/get_code_from_twillo')
                 .then((response) => {
                     console.log(response.body[0].body.substring(0, 6));
                     let code = response.body[0].body.substring(0, 6);
                     cy.log(code);
                     cy.wait(5000);
-                    cy.get('input[id="secure-account-code"]').type(code, {force: true});
-                    cy.get('.button').click({force: true});
-                    cy.wait(3000);
+                    cy.get('input[id="signin-otp"]').type(code, {force: true});
                     cy.get('.button').click({force: true});
                 });
+            cy.get('a[href="/agent/dashboard/client/f1c4e600-2ede-4ac3-af21-d9d086cfd700"]').click({force: true});
+            cy.wait(2000);
+            cy.get('.back-button').click({force: true});
+        });
+    });
+
+    it('SigninClient', function () {
+        cy.fixture('secret').then((secretData) => {
+            cy.log("Go to Home page");
+            cy.visit(secretData.ruusterHomePageSignin);
+            cy.get('input[name="email"]').type(secretData.clientEmail, {force: true});
+            cy.get('input[name="password"]').type(secretData.clientPass, {force: true});
+            cy.get('button[type="submit"]').click({force: true});
+            cy.wait(5000);
+            cy.request('http://localhost:3000/get_code_from_twillo')
+                .then((response) => {
+                    console.log(response.body[0].body.substring(0, 6));
+                    let code = response.body[0].body.substring(0, 6);
+                    cy.log(code);
+                    cy.wait(5000);
+                    cy.get('input[id="signin-otp"]').type(code, {force: true});
+                    cy.get('.button').click({force: true});
+                });
+            cy.get('a[href="/budget/affordability/sample"]').click({force: true});
+            cy.get('input[id="property-zip"]').clear({force: true});
+            cy.get('input[id="property-zip"]').type("90005", {force: true});
+            cy.get('a[href="/education"]').click({force: true});
+            cy.wait(1000);
+            cy.get('.articles--item-read').eq(0).click({force: true});
+            cy.wait(2000);
+        });
+    });
+
+    it('checkNotification', function () {
+        cy.fixture('secret').then((secretData) => {
+            cy.log("Go to Home page");
+            cy.visit(secretData.ruusterHomePageSignin);
+            cy.get('input[name="email"]').type(secretData.agentEmail, {force: true});
+            cy.get('input[name="password"]').type(secretData.agentPass, {force: true});
+            cy.get('button[type="submit"]').click({force: true});
+            cy.wait(5000);
+            cy.request('http://localhost:3000/get_code_from_twillo')
+                .then((response) => {
+                    console.log(response.body[0].body.substring(0, 6));
+                    let code = response.body[0].body.substring(0, 6);
+                    cy.log(code);
+                    cy.wait(5000);
+                    cy.get('input[id="signin-otp"]').type(code, {force: true});
+                    cy.get('.button').click({force: true});
+                });
+            cy.get('.agent-dashboard--client-activity-user-item-counter').contains('2');git
         });
     });
 });
+
+
+
+
+
